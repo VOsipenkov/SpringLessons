4570bb370bddf86bf4c5d9dba7da70c8c781a118
Vladislav Osipenkov <JumpViaOcean@gmail.com>
2016-12-17 15:03:43 +0400

    lesson 7 final

--- src/main/java/com/yet/spring/core/App.java
+++ src/main/java/com/yet/spring/core/App.java
@@ -2,35 +2,44 @@ package com.yet.spring.core;
 
 import java.sql.Date;
 import java.text.DateFormat;
+import java.util.Map;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.yet.spring.loggers.EventLogger;
 
 public class App {
-    static Client client;
-    static EventLogger logger;
+	private static Client client;
+	private static Map<EventType, EventLogger> mapLoggers;
+	private static EventLogger defaultLogger;
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext contex = new ClassPathXmlApplicationContext("spring.xml");
-        App app = (App) contex.getBean("app");
-        app.logEvent("log event from 1 \n", EventType.INFO);
-        app.logEvent("log event from 2 \n", EventType.INFO);
+	public static void main(String[] args) {
+		ConfigurableApplicationContext contex = new ClassPathXmlApplicationContext("spring.xml");
+		App app = (App) contex.getBean("app");
+		app.logEvent("log event from 1 \n", EventType.ERROR);
+		app.logEvent("log event from 2 \n", EventType.INFO);
+		app.logEvent("log event from 3 \n", EventType.ERROR);
 
-        contex.close();
-    }
+		contex.close();
+	}
 
-    public App(Client client, EventLogger logger) {
-        this.client = client;
-        this.logger = logger;
-    }
+	public App(Client client, EventLogger defauleLogger, Map<EventType, EventLogger> mapLoggers) {
+		this.client = client;
+		this.mapLoggers = mapLoggers;
+		this.defaultLogger = defauleLogger;
+	}
 
-    private void logEvent(String message, EventType eventType) {
-        String fixedMessage = message.replaceAll(client.getId(), client.getName());
-        Date currentDate = new Date(System.currentTimeMillis());
-        Event event = new Event(currentDate);
-        DateFormat df = DateFormat.getDateInstance();
-        event.setMessage(fixedMessage, df);
+	private void logEvent(String message, EventType eventType) {
+		String fixedMessage = message.replaceAll(client.getId(), client.getName());
+		Date currentDate = new Date(System.currentTimeMillis());
+		Event event = new Event(currentDate);
+		DateFormat df = DateFormat.getDateInstance();
+		event.setMessage(fixedMessage, df);
 
-        logger.logEvent(event);
-    }
+		EventLogger logger = mapLoggers.get(eventType);
+		if (logger == null) {
+			logger = defaultLogger;
+		}
+
+		logger.logEvent(event);
+	}
 }
--- src/main/java/com/yet/spring/loggers/CombinedEventLogger.java
+++ src/main/java/com/yet/spring/loggers/CombinedEventLogger.java
@@ -1,15 +1,19 @@
 package com.yet.spring.loggers;
 
-import java.util.Collection;
-
+import java.util.List;
 import com.yet.spring.core.Event;
 
 public class CombinedEventLogger implements EventLogger {
 
-    private Collection loggers;
+	private List<EventLogger> loggers;
 
-    public void logEvent(Event event) {
-        // TODO Auto-generated method stub
-    }
+	public CombinedEventLogger(List<EventLogger> loggers) {
+		this.loggers = loggers;
+	}
 
+	public void logEvent(Event event) {
+		for (EventLogger loggerItem : loggers) {
+			loggerItem.logEvent(event);
+		}
+	}
 }
